805aa4a32a3e8ee1a5f5b6e2648c0549a53b4b94
Will Hinkelman <will.hink24@gmail.com>
2020-05-18 13:00:11 -0700

    menu is hidden with burger

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ const dropDown = document.querySelector('.navbar-menu');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('is-active');
-    burger.classList.toggle('is-active');
+    // burger.classList.toggle('is-active');
     dropDown.classList.toggle('is-active');
 })
